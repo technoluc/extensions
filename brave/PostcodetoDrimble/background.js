16d324186173a2e0c92ee8dd005217cb7ce4863b
TechnoLuc <94373293+technoluc@users.noreply.github.com>
2023-10-19 14:38:04 +0200

    add send to KvK

--- brave/PostcodetoDrimble/background.js
+++ brave/PostcodetoDrimble/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "customLink",
-    title: "Postcode to Drimble",
+    title: "Search on Drimble for '%s'",
     contexts: ["selection"],
   });
 });
